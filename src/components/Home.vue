007055acd2bcc33e82592aa8f0c98a1857b9ca19
c711cat <c711cat@gmail.com>
2022-06-08 09:27:16 +0800

    加入起始畫面元件

---
+++ src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="getting-start-wrap p-d-flex p-jc-center p-ai-center">
+    <div>
+      <img class="img-style p-mb-3" src="@/img/img_default.png" alt="" />
+      <div class="getting-start-text p-text-bold">開始使用Cherri Chat!</div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.getting-start-wrap {
+  height: 700px;
+}
+
+.img-style {
+  width: 200px;
+}
+
+.getting-start-text {
+  color: #4a90e2;
+  font-size: 22px;
+}
+</style>
