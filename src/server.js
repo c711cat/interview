b2501f32c62230e622493190d80bbbfb6efc4986
c711cat <c711cat@gmail.com>
2022-06-09 13:39:09 +0800

    設定打API會取得的對話資料

--- src/server.js
+++ src/server.js
@@ -10,10 +10,24 @@ export function makeServer() {
     routes() {
       this.get("/friends", () => {
         return [
-          { name: "保羅", introduction: "大家好，我是保羅~" },
-          { name: "傑克", introduction: "大家好，我是傑克~" },
-          { name: "傑森", introduction: "大家好，我是傑森~" },
+          { id: "1", name: "保羅", introduction: "大家好，我是保羅~" },
+          { id: "2", name: "傑克", introduction: "大家好，我是傑克~" },
+          { id: "3", name: "傑森", introduction: "大家好，我是傑森~" },
         ];
+      });
+      this.get("/conversation/1", () => {
+        return [
+          "保羅",
+          "你好，我是潔西卡",
+          "我喜歡吃的食物有",
+          "各種巧克力口味的甜點",
+        ];
+      });
+      this.get("/conversation/2", () => {
+        return ["傑克", "你好，我是潔西卡", "我喜歡做的運動為", "游泳,跑步"];
+      });
+      this.get("/conversation/3", () => {
+        return ["傑森", "你好，我是潔西卡", "我喜歡的動物為", "貓,狗"];
       });
     },
   });
